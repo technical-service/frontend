c6d5f76fa931d50535e91cfba0d9857bfb28b454
technical-service <105105679+technical-service@users.noreply.github.com>
2022-05-09 18:49:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+require("./db/mongoose");
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+
+const app = express();
+// Assign the 3001 port for run
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
