e56acc03e7e357922028dca7786beb82141d1d02
technical-service <105105679+technical-service@users.noreply.github.com>
2022-05-10 17:47:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const app = express();
-// Assign the 3001 port for run
-const port = process.env.PORT || 3001;
+// Assign the 3000 port for run the application
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(userRouter);
