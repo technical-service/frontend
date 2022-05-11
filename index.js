078768c17925545a4cb11f31503a0fa176d61842
technical-service <105105679+technical-service@users.noreply.github.com>
2022-05-11 11:54:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,3 +34,7 @@ app.listen(port, () => {
 app.listen(port, () => {
   console.log("Server is running on port " + port);
 });
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
