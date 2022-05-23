4de7924495c2815d2c0c487da2a355bf4af313c8
technical-service <105105679+technical-service@users.noreply.github.com>
2022-05-23 20:14:33 +0530

    Committed

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log("Server is running on port " + port);
 });
+
+app.listen(port, () => {
+  console.log("Server is running on port " + port);
+});
